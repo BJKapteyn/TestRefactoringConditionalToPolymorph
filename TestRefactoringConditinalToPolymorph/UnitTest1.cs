40c0199e48ca410222113e27f2bce4712a98088f
Brad <bjkapteyn@gmail.com>
2019-07-23 17:47:07 -0400

    Couldnt get the tests to work with the objects but it's dinner time

--- TestRefactoringConditinalToPolymorph/UnitTest1.cs
+++ TestRefactoringConditinalToPolymorph/UnitTest1.cs
@@ -9,34 +9,30 @@ namespace TestRefactoringConditinalToPolymorph
         public void Test1()
         {
             string tester = "Hello";
+            Person p = new Person();
 
-            Assert.Equal(tester, GetName("Person"));
+            Assert.Equal(tester, SayWhat(p));
         }
-
+        public static Person person = new Person();
         [Theory]
-        [InlineData("Person", "Hello")]
-        [InlineData("Dog", "Bark")]
-        [InlineData("Cat", "Meow")]
-        [InlineData("stsdfse", "Error")]
-        public void Test2(string user, string expected)
+        [InlineData(new Person(), "Hello")]
+        [InlineData(new Dog(), "Bark")]
+        [InlineData(new Cat(), "Meow")]
+        public void Test2(Animal user, string expected)
         {
-            Assert.Equal(GetName(user), expected);
+            Assert.Equal(expected, SayWhat(user));
         }
 
-        public string GetName(string whoIsIt)
+        public string SayWhat(Animal a)
         {
-            switch(whoIsIt)
-            {
-                case "Human":
-                    return "Hello";
-                case "Dog":
-                    return "Bark";
-                case "Cat":
-                    return "Meow";
-                default:
-                    return "Error";
-            }
+            return a.Talk();
         }
+    }
+    public class Users
+    {
+        public static Person person = new Person();
+        public static Cat cat = new Cat();
+        public static Dog dog = new Dog();
     }
     public abstract class Animal
     {
